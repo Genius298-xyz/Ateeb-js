2d73ede11d27568419a5eec2a0ca0b0bb775a9fe
Genius298-xyz <iamateeb18@gmail.com>
2026-06-11 07:54:01 +0000

    functions

---
+++ 2_Basic/04_Function.js
@@ -0,0 +1,16 @@
+function SayMyName(){
+    console.log("A")
+    console.log("T")
+    console.log("E")
+    console.log("E")
+    console.log("B")
+}
+
+SayMyName()
+
+function Add2Nos(number1, number2)
+{
+console.log(number1 + number2)
+}
+
+Add2Nos(3, 66)
